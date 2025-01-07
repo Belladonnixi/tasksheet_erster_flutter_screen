99bb6a9a2a0a4a8906bcf84c915a34cf5790cf1f
Jessica Ernst <jessica_ernst@outlook.de>
2025-01-07 16:12:10 +0100

    update material app title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PSheet: erster Flutter Screen',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(255, 10, 91, 111),
